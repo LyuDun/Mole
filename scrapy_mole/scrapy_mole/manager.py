f74c93e34cae94028a2cf2702be3cb6c47d6f1aa
LvDunn <cqcqhelloworld@gmail.com>
2020-01-15 01:16:19 +0800

    20200115

--- scrapy_mole/scrapy_mole/manager.py
+++ scrapy_mole/scrapy_mole/manager.py
@@ -1,6 +1,7 @@
 from ext import db, Redis
+from scrapy.cmdline import execute
+from threading import Timer
 cursor = db.cursor()
-
 
 def manage_url():
     sql = """SELECT product_url FROM mole_product WHERE product_status = '00' """
@@ -12,6 +13,18 @@ def manage_url():
             Redis.lpush('sephora:start_urls', url)
     except Exception as e:
         print(e)
+    finally:
+        print('1')
+
+
+class Time_task(Timer):
+    def run(self):
+        while not self.finished.is_set():
+            self.function(*self.args, **self.kwargs)
+            self.finished.wait(self.interval)
+
 
 if __name__ == '__main__':
-    manage_url()
+    t = Time_task(10, manage_url)
+    t.start()
+    execute(['scrapy', 'crawl', 'sephora'])
